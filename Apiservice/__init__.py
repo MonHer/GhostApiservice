54973ddc60295ae6bcfc35de606bea73fdb58bc0
GhostGuanyin <40558379+MonHer@users.noreply.github.com>
2019-11-21 02:22:26 +0800

    First submission

---
+++ Apiservice/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#@Pjname ;GhostApiservice
+#@Time   :2019/11/21/02:01
+#@Author :GhostGuanyin
+#@File   :__init__.py.py
+
+__version__ = "0.1.0"
---
+++ Apiservice/test_Apiservice.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#@Pjname ;GhostApiservice
+#@Time   :2019/11/21/02:02
+#@Author :GhostGuanyin
+#@File   :test_Apiservice.py
+
+def test_version():
+    from Apiservice import __version__
+    assert isinstance(__version__,str)
