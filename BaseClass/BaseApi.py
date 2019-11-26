6b5505f03254efbf44ad8210e0c75f2879b0fa52
GhostGuanyin <40558379+MonHer@users.noreply.github.com>
2019-11-26 22:20:05 +0800

    Optimize boby's json inspection writing

--- BaseClass/BaseApi.py
+++ BaseClass/BaseApi.py
@@ -40,14 +40,15 @@ class BaseApi(object):
     def validate(self, key, expected_value):
         value = self.response
         for _key in key.split('.'):
-            # print("value====",_key,value)
+            # print("value====",_key,value)打印调试信息
             if isinstance(value,requests.Response):
-                if _key == "json()":
+                if _key == "json()":  #可以写成if _key in ["json()","json"]
                     value = self.response.json()
                 else:
                     value = getattr(value, _key)
             elif isinstance(value,(requests.structures.CaseInsensitiveDict,dict)):
                 value = value[_key]
-        print("value====", _key, value,expected_value)
+        # print("value====", _key, value,expected_value)打印调试信息
+
         assert value == expected_value
         return self
